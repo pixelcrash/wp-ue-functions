4f7ce97ae495b303d8264709f1f2509d21c7adc4
Georg Kettele <kg@kmkg.org>
2018-01-26 16:35:40 +0000

    inital commit

---
+++ list-parents-child-categories.php
@@ -0,0 +1,31 @@
+function ue_list_cats($id, $links=1, $seperator=", "){
+
+$categories = get_the_category($id);
+if( $categories ){
+    $output = "";
+
+    // THE PARENTS FIRST 
+    foreach ($categories as $category) {
+        if( !$category->parent ){
+            if($links){
+              $output .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" title="' . esc_attr( sprintf( __( "View all posts in %s" ), $category->name ) ) . '" >' . $category->name.'</a>' . $seperator;
+            }else{
+              $output .= $category->name . $seperator;
+            }
+        }
+    }
+
+    // CHILD CATEGORIES
+    foreach ($categories as $category) {
+        if( $category->parent ){
+          if($links){
+            $output .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" title="' . esc_attr( sprintf( __( "View all posts in %s" ), $category->name ) ) . '" >' . $category->name.'</a>' . $seperator;
+          }else{
+            $output .= $category->name. $seperator;
+          }
+            
+        }
+    }
+    
+    echo trim($output, $seperator);
+}
